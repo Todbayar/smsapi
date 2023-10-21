59dc29cd222af68a665758eb6130562b07bbb0e6
Todbayar <atodko0513@yahoo.com>
2023-10-21 12:24:28 +0800

    updated instructions

--- instructions.php
+++ instructions.php
@@ -3,74 +3,41 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="section-title">
-					<h2>Заавар</h2>
+					<h2>API заавар</h2>
 					<img src="img/separator.png" style="width:250px; height:2px">
-					<p>SMS gateway хэрхэн ашиглах тухай заавар</p>
+					<p>SMS api хэрхэн ашиглах тухай заавар</p>
 					<p>Анхаарах! Крилл үсгээр мессеж илгээх боломжгүй.</p>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont-code-alt"></i>
-					<h4>SMS илгээх</h4>
-					<p>
-						http://localhost/sms_gateway/process.php<br/>
-						form_data:<br/>
-						action:sms_send<br/>
-						phone:хүлээн авагчын утасны дугаар (жишээ нь:99213557)
-						msg:ene bol huleen avagchid ilgeej bui msg
-					</p>
-						
-				</div>
-				<!-- End Single Service -->
-			</div>
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont icofont-tooth"></i>
-					<h4><a href="service-details.html">Teeth Whitening</a></h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
-				</div>
-				<!-- End Single Service -->
-			</div>
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont icofont-heart-alt"></i>
-					<h4><a href="service-details.html">Heart Surgery</a></h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
-				</div>
-				<!-- End Single Service -->
-			</div>
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont icofont-listening"></i>
-					<h4><a href="service-details.html">Ear Treatment</a></h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
-				</div>
-				<!-- End Single Service -->
-			</div>
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont icofont-eye-alt"></i>
-					<h4><a href="service-details.html">Vision Problems</a></h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
-				</div>
-				<!-- End Single Service -->
-			</div>
-			<div class="col-lg-4 col-md-6 col-12">
-				<!-- Start Single Service -->
-				<div class="single-service">
-					<i class="icofont icofont-blood"></i>
-					<h4><a href="service-details.html">Blood Transfusion</a></h4>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus dictum eros ut imperdiet. </p>	
-				</div>
-				<!-- End Single Service -->
+			<!-- Start Single Service -->
+			<div class="single-service" style="padding-left:15px; padding-right:15px">
+				<h4>SMS текст илгээх</h4>
+				<p>Доорх өгөгдлийг <a style="color:#e83e8c">http://localhost/sms_gateway/process.php</a>-руу <a style="color:#1A76D1; font-weight: bold">post</a> хүсэлт илгээх.</p><br/>
+				<h5>form datas</h5><br/>
+				<table class="instruction">
+					<tr>
+						<th width="30%">Параметер</th>
+						<th width="30%">Утга</th>
+						<th width="40%">Тайлбар</th>
+					</tr>
+					<tr>
+						<td>action</td>
+						<td>sms_text_send</td>
+						<td>Мессеж төрөл</td>
+					</tr>
+					<tr>
+						<td>phone</td>
+						<td>99213557</td>
+						<td>хүлээн авагчын утасны дугаар</td>
+					</tr>
+					<tr>
+						<td>msg</td>
+						<td>sain baina uu? ene bol test sms msg</td>
+						<td>Хүлээн авагчруу илгээх текст мессеж</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 	</div>
