30b65869e8bce18abe3471f4190c2dfd277641e0
Todbayar <atodko0513@yahoo.com>
2023-10-21 19:50:50 +0800

    updated signin, signup form

--- signin.php
+++ signin.php
@@ -1,31 +1,61 @@
-<div class="row">
-	<div class="col-lg-4 col-md-4 col-12">
-		<div class="form-group">
-			<i class="fa fa-user"></i>
-			<input type="text" name="first-name" placeholder="First name" required="required">
+<script>
+function showSignup(){
+	$(".signin-form").hide();
+	$(".signup-form").show();
+}
+	
+function showSignin(){
+	$(".signin-form").show();
+	$(".signup-form").hide();
+}
+</script>
+<section class="news-single section">
+	<div class="container">
+		<div class="row" style="max-width: 540px; margin-left: auto; margin-right: auto">
+			<div class="col-lg-8 col-12">
+				<div class="row">
+					<div class="col-12">
+						<div class="signin-form">
+							<h2>Имейлээр нэвтрэх</h2>
+							<div class="row" style="margin-left: 5px">
+								<div class="form-group" style="width: 95%">
+									<i class="icofont-ui-email"></i>
+									<input type="text" name="user_signin_email" placeholder="Имейл" required="required">
+								</div>
+								<div class="form-group" style="width: 95%">
+									<i class="icofont-ui-password"></i>
+									<input type="text" name="user_signin_password" placeholder="Нууц үг" required="required">
+								</div>
+								<div class="form-group button">	
+									<button type="submit" class="btn primary">OK</button>
+									<a href="javascript:showSignup()" style="margin-left: 10px">Шинээр бүртгүүлэх</a>
+								</div>
+							</div>
+						</div>
+						<div class="signup-form" style="display: none">
+							<h2>Имейлээр бүртгүүлэх</h2>
+							<div class="row" style="margin-left: 5px">
+								<div class="form-group" style="width: 95%">
+									<i class="icofont-ui-email"></i>
+									<input type="text" name="user_signup_email" placeholder="Имейл" required="required">
+								</div>
+								<div class="form-group" style="width: 95%">
+									<i class="icofont-ui-password"></i>
+									<input type="text" name="user_signup_password1" placeholder="Нууц үг" required="required">
+								</div>
+								<div class="form-group" style="width: 95%">
+									<i class="icofont-ui-password"></i>
+									<input type="text" name="user_signup_password2" placeholder="Давтан Нууц үг" required="required">
+								</div>
+								<div class="form-group button">	
+									<button type="submit" class="btn primary">OK</button>
+									<a href="javascript:showSignin()" style="margin-left: 10px">Буцах</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="col-lg-4 col-md-4 col-12">
-		<div class="form-group">
-			<i class="fa fa-envelope"></i>
-			<input type="text" name="last-name" placeholder="Last name" required="required">
-		</div>
-	</div>
-	<div class="col-lg-4 col-md-4 col-12">
-		<div class="form-group">
-			<i class="fa fa-envelope"></i>
-			<input type="email" name="email" placeholder="Your Email" required="required">
-		</div>
-	</div>
-	<div class="col-12">
-		<div class="form-group message">
-			<i class="fa fa-pencil"></i>
-			<textarea name="message" rows="7" placeholder="Type Your Message Here"></textarea>
-		</div>
-	</div>
-	<div class="col-12">
-		<div class="form-group button">	
-			<button type="submit" class="btn primary"><i class="fa fa-send"></i>Submit Comment</button>
-		</div>
-	</div>
-</div>
+</section>
