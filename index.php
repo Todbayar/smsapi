46479288b9eef42aa5445d8ab8d1cedeb47891c6
Todbayar <atodko0513@yahoo.com>
2023-10-21 15:49:17 +0800

    signin update

--- index.php
+++ index.php
@@ -45,6 +45,67 @@ require "language/mon.php";
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="css/responsive.css">
 		
+		<!-- jquery Min JS -->
+        <script src="js/jquery.min.js"></script>
+		<!-- jquery Migrate JS -->
+		<script src="js/jquery-migrate-3.0.0.js"></script>
+		<!-- jquery Ui JS -->
+		<script src="js/jquery-ui.min.js"></script>
+		<!-- Easing JS -->
+        <script src="js/easing.js"></script>
+		<!-- Color JS -->
+		<script src="js/colors.js"></script>
+		<!-- Popper JS -->
+		<script src="js/popper.min.js"></script>
+		<!-- Bootstrap Datepicker JS -->
+		<script src="js/bootstrap-datepicker.js"></script>
+		<!-- Jquery Nav JS -->
+        <script src="js/jquery.nav.js"></script>
+		<!-- Slicknav JS -->
+		<script src="js/slicknav.min.js"></script>
+		<!-- ScrollUp JS -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Niceselect JS -->
+		<script src="js/niceselect.js"></script>
+		<!-- Tilt Jquery JS -->
+		<script src="js/tilt.jquery.min.js"></script>
+		<!-- Owl Carousel JS -->
+        <script src="js/owl-carousel.js"></script>
+		<!-- counterup JS -->
+		<script src="js/jquery.counterup.min.js"></script>
+		<!-- Steller JS -->
+		<script src="js/steller.js"></script>
+		<!-- Wow JS -->
+		<script src="js/wow.min.js"></script>
+		<!-- Magnific Popup JS -->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<!-- Counter Up CDN JS -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- Main JS -->
+		<script src="js/main.js"></script>
+		
+		<script>
+		var prevPage = "index.php";
+		$(document).ready(function(){
+			currentActivePage();
+		});
+			
+		function currentActivePage(){
+			let paramString = window.location.href.split('?')[1];
+			let queryString = new URLSearchParams(paramString);
+			for (let pair of queryString.entries()) {
+				if(pair[0]=="page"){
+					$("li."+prevPage.substr(0, prevPage.indexOf('.'))).removeClass("active");
+					console.log("<test>:"+"li."+pair[1].substr(0, pair[1].indexOf('.')));
+					$("li."+pair[1].substr(0, pair[1].indexOf('.'))).addClass("active");
+					prevPage = pair[1];
+				}
+			}
+		}
+		</script>
+		
     </head>
     <body>
 	
@@ -109,11 +170,21 @@ require "language/mon.php";
 								<div class="main-menu">
 									<nav class="navigation">
 										<ul class="nav menu">
-											<li class="active"><a href="index.php">Нүүр</i></a></li>
-											<li><a href="?page=signin.php">Нэвтрэх</a></li>
-											<li><a href="?page=instructions.php">Заавар</a></li>
-											<li><a href="?page=profile">Хэрэглэгч</a></li>
-											<li><a href="?page=price.php">Төлбөр</a></li>
+											<?php
+											if(!isset($_COOKIE["userID"])){
+											?>
+											<li class="index active"><a href="index.php">Нүүр</i></a></li>
+											<li class="signin"><a href="?page=signin.php">Нэвтрэх</a></li>
+											<?php
+											}
+											else {
+											?>
+											<li class="instructions"><a href="?page=instructions.php">Заавар</a></li>
+											<li class="profile"><a href="?page=profile">Хэрэглэгч</a></li>
+											<li class="price"><a href="?page=price.php">Төлбөр</a></li>
+											<?php
+											}
+											?>
 										</ul>
 									</nav>
 								</div>
@@ -173,46 +244,5 @@ require "language/mon.php";
 			<!--/ End Copyright -->
 		</footer>
 		<!--/ End Footer Area -->
-		
-		<!-- jquery Min JS -->
-        <script src="js/jquery.min.js"></script>
-		<!-- jquery Migrate JS -->
-		<script src="js/jquery-migrate-3.0.0.js"></script>
-		<!-- jquery Ui JS -->
-		<script src="js/jquery-ui.min.js"></script>
-		<!-- Easing JS -->
-        <script src="js/easing.js"></script>
-		<!-- Color JS -->
-		<script src="js/colors.js"></script>
-		<!-- Popper JS -->
-		<script src="js/popper.min.js"></script>
-		<!-- Bootstrap Datepicker JS -->
-		<script src="js/bootstrap-datepicker.js"></script>
-		<!-- Jquery Nav JS -->
-        <script src="js/jquery.nav.js"></script>
-		<!-- Slicknav JS -->
-		<script src="js/slicknav.min.js"></script>
-		<!-- ScrollUp JS -->
-        <script src="js/jquery.scrollUp.min.js"></script>
-		<!-- Niceselect JS -->
-		<script src="js/niceselect.js"></script>
-		<!-- Tilt Jquery JS -->
-		<script src="js/tilt.jquery.min.js"></script>
-		<!-- Owl Carousel JS -->
-        <script src="js/owl-carousel.js"></script>
-		<!-- counterup JS -->
-		<script src="js/jquery.counterup.min.js"></script>
-		<!-- Steller JS -->
-		<script src="js/steller.js"></script>
-		<!-- Wow JS -->
-		<script src="js/wow.min.js"></script>
-		<!-- Magnific Popup JS -->
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<!-- Counter Up CDN JS -->
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
-		<!-- Bootstrap JS -->
-		<script src="js/bootstrap.min.js"></script>
-		<!-- Main JS -->
-		<script src="js/main.js"></script>
     </body>
 </html>
