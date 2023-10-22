ae1883ab3080cd693007552172b96108f57d3209
Todbayar <atodko0513@yahoo.com>
2023-10-22 15:55:33 +0800

    updated price plan

--- price.php
+++ price.php
@@ -19,9 +19,31 @@
 						<div class="icon">
 							<img src="img/logo_sms_small.png" />
 						</div>
-						<h4 class="title">1000 кредит</h4>
+						<h4 class="title">Starter plan</h4>
+						<h4 class="title">140 кредит</h4>
 						<div class="price">
-							<p class="amount">2500₮<span></span></p>
+							<p class="amount">7000₮<span>/50₮</span></p>
+						</div>
+					</div>
+					<div class="table-bottom">
+						<a class="btn" href="#">Цэнэглэх</a>
+					</div>
+					<!-- Table Bottom -->
+				</div>
+			</div>
+			<!-- End Single Table-->
+			<!-- Single Table -->
+			<div class="col-lg-4 col-md-12 col-12">
+				<div class="single-table">
+					<!-- Table Head -->
+					<div class="table-head">
+						<div class="icon">
+							<img src="img/logo_sms_small.png" />
+						</div>
+						<h4 class="title">Mid plan</h4>
+						<h4 class="title">250 кредит</h4>
+						<div class="price">
+							<p class="amount">10000₮<span>/40₮</span></p>
 						</div>	
 					</div>
 					<div class="table-bottom">
@@ -39,29 +61,10 @@
 						<div class="icon">
 							<img src="img/logo_sms_small.png" />
 						</div>
-						<h4 class="title">2000 кредит</h4>
+						<h4 class="title">Pro plan</h4>
+						<h4 class="title">800 кредит</h4>
 						<div class="price">
-							<p class="amount">7000₮<span></span></p>
-						</div>	
-					</div>
-					<div class="table-bottom">
-						<a class="btn" href="#">Цэнэглэх</a>
-					</div>
-					<!-- Table Bottom -->
-				</div>
-			</div>
-			<!-- End Single Table-->
-			<!-- Single Table -->
-			<div class="col-lg-4 col-md-12 col-12">
-				<div class="single-table">
-					<!-- Table Head -->
-					<div class="table-head">
-						<div class="icon">
-							<img src="img/logo_sms_small.png" />
-						</div>
-						<h4 class="title">5000 кредит</h4>
-						<div class="price">
-							<p class="amount">20000₮<span></span></p>
+							<p class="amount">20000₮<span>/25₮</span></p>
 						</div>	
 					</div>
 					<div class="table-bottom">
